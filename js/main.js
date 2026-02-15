3c128f7bcb7f85a81719e5c1b17c9d678e41fc1e
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-02-15 15:07:03 -0300

    pagina de alunos em andamento, os cards ainda nao se alinham em colunas

--- js/main.js
+++ js/main.js
@@ -9,10 +9,74 @@ import{
 
 const main = document.querySelector('main')
 const container = document.getElementById('container')
+const btnCursos = document.getElementById('cursos')
+
+async function carregarCursos() {
+
+    const cursos = await getCursos()
+
+    cursos.forEach(curso => {
+
+        const botao = document.createElement('button')
+        botao.innerHTML = `
+            <span>${curso.sigla}</span>
+        `;
+
+        botao.addEventListener('click', () => {
+            carregarAlunos(curso.id)
+        })
+
+        btnCursos.appendChild(botao)
+
+    })
+    
+}
+
+carregarCursos()
 
 async function carregarAlunos(id_curso){
+
+    main.classList.remove('home')
+    main.classList.add('alunos')
+
     const alunos = await getAlunosByCurso(id_curso)
 
-    
+    alunos.forEach(aluno => {
+        
+        const card = document.createElement('div')
+        card.classList.add('cards')
 
+        card.innerHTML = `
+            <img src="${aluno.foto}" alt="">
+            <span>${aluno.nome}</span>
+        `;
+
+        card.addEventListener('click', () => {
+            carregarInfoAluno(aluno.id)
+        })
+
+        container.appendChild(card)
+    });
+}
+
+async function carregarInfoAluno(id_aluno){
+
+    const aluno = getAlunosById(id_aluno)
+
+    aluno.forEach(infoAluno => {
+        const card = document.createElement('div')
+        card.classList.add('card-aluno')
+        const info = document.createElement('div')
+        info.classList.add('info-aluno')
+
+        card.innerHTML = `
+            <img src="${infoAluno.foto}" alt="">
+            <span>${infoAluno.nome}</span>
+        `;
+
+        info.innerHTML = `
+            
+        `;
+
+    })
 }
